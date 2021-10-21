a4b95eb7b50167509d55bd07224d2b04084244a3
Eduardo Wronscki <eduwric@gmail.com>
2021-10-20 22:11:10 -0300

    feat: add carousel to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface IDocuments{
+  src:string;
+  name:string;
+  description:string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-carousel';
+
+  public slides: IDocuments[] = [
+    {
+      src:
+        'https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+      name: 'burger 1 ',
+      description: 'food food food food',
+    },
+    {
+      src:
+        'https://images.unsplash.com/photo-1460306855393-0410f61241c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80',
+      name: 'burger2',
+      description: 'food food food food',
+    },
+    {
+      src:
+        'https://images.unsplash.com/photo-1498579485796-98be3abc076e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+      name: 'burger 3',
+      description: 'food food food food',
+    },
+  ];
 }
